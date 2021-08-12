c4773ad2e50c35f3e21c7d148ede4cdb59afe4f9
GGGGGHT <wz9712203617@gmail.com>
2021-08-12 09:17:00 +0800

    feat: 读进10个f loat值，用指针将它们存放在一个程序块里

--- alloc.c
+++ alloc.c
@@ -34,13 +34,33 @@ int main(int argc, char *argv[]) {
     p -= 2;
     // 释放内存的语句free(p) `中的参数p，必须是申请到的动态内存的首地址。
     free(p);*/
-    char *p,*q;
-    p = q = (char *) malloc(100);
-    scanf("%s", q);
-    while (*p != '\0')
-        p++;
-    printf("%lld\n", p - q);
-    // 使用free时要注意,指向同一个目标的指针只能释放一次,因为释放后另一个已经是空指针,再free就要出错
-    free(q);
+//    char *p,*q;
+//    p = q = (char *) malloc(100);
+//    scanf("%s", q);
+//    while (*p != '\0')
+//        p++;
+//    printf("%lld\n", p - q);
+//    // 使用free时要注意,指向同一个目标的指针只能释放一次,因为释放后另一个已经是空指针,再free就要出错
+//    free(q);
+
+    float *p, *q;
+    p = q = (float *) malloc(10 * sizeof(float));
+    if (q == NULL) {
+        printf("error");
+        return -1;
+    }
+
+    for (int i = 0; i < 10; i++,q++)
+        scanf("%f", q);
+    float min = 0xffffff, sum = 0;
+    for (int i = 0; i < 10; ++i, ++p) {
+        sum += *p;
+        if (min > *p) {
+            min = *p;
+        }
+    }
+
+    printf("min: %f,sum: %f", min, sum);
+
     return 0;
 }

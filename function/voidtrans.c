99b07d2c34a12c948ce08bc27f6a8bdf370da852
ggggght <wz9712203617@gmail.com>
2021-08-16 10:48:18 +0800

    feat

--- function/voidtrans.c
+++ function/voidtrans.c
@@ -2,6 +2,7 @@
 
 void add(double,double,double *);
 
+// add函数里的指针q指向主函数变量c的存储地址，*q与c同步变化。调用结束后，系统回收为add函数里形参分配的地址，形参变量也随之消失，而c和*q是同一个地址，所以虽然q消失了，但c却保持为原来*q的值。
 void main() {
     double a = 5.5,b = 15.39, c= 0;
     add (a,b,&c);

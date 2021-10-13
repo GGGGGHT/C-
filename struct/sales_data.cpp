5bfab7ab6d14ffb5818822c45b9997869da3e1be
ggggght <wz9712203617@gmail.com>
2021-10-13 11:26:44 +0800

    feat: use string lib function

--- struct/sales_data.cpp
+++ struct/sales_data.cpp
@@ -3,21 +3,65 @@
 
 struct Sales_data {
     std::string bookNo;
-   unsigned units_sold ;
+    unsigned units_sold;
     double revenue;
 };
 
+using namespace std;
 int main() {
-    Sales_data s1,s2;
+//    Sales_data s1,s2;
+//
+//    double price = 0;
+//    std::cin >> s1.bookNo >> s1.units_sold >> price;
+//    s1.revenue = s1.units_sold * price;
+//    std::cout << s1.revenue << std::endl;
+//    std::cin >> s2.bookNo >> s2.units_sold >> price;
+//    s2.revenue = s2.units_sold * price;
+//
+//    if(s1.bookNo == s2.bookNo) {
+//        unsigned totalCnt = s1.units_sold + s2.units_sold;
+//    }
+//    string hello = "Hello";
+//    string h2 = "Hello world";
+//    cout << (hello > h2) << endl;
+//    string s1 = "hello";
+//    string s2 = "world";
+//    string s3 = s1 + ", " + s2 + '\n';
+//    cout << s3;
 
-    double price = 0;
-    std::cin >> s1.bookNo >> s1.units_sold >> price;
-    s1.revenue = s1.units_sold * price;
-    std::cout << s1.revenue << std::endl;
-    std::cin >> s2.bookNo >> s2.units_sold >> price;
-    s2.revenue = s2.units_sold * price;
+//    string str = "Hello world";
+//    for (auto s : str) {
+//        cout << s << endl;
+//    }
+//
+//    string s("Hello world!!!");
+//    decltype(s.size()) punct_cnt = 0;
+//    for (auto c : s) {
+//        if (ispunct(c)) {
+//            ++punct_cnt;
+//        }
+//    }
+//
+//    cout <<punct_cnt << " punctuation characters in " << s << endl;
+//    while (cin >> world) {
+//        auto len = world.size();
+//
+//        if(len > 3)
+//            cout << world << endl;
+//    }
 
-    if(s1.bookNo == s2.bookNo) {
-        unsigned totalCnt = s1.units_sold + s2.units_sold;
+    string s("Hello World!!!");
+    for(auto &c: s) {
+        c = toupper(c);
     }
+
+    cout << s << endl;
+    if(!s.empty()) {
+//        cout << s[0] << endl;
+        s[0] = tolower(s[0]);
+    }
+    cout << s << endl;
+
+
+    return 0;
 }

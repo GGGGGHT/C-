bbb5e61740a04e9e40cfea4a93a90b9a177492ed
ggggght <wz9712203617@gmail.com>
2021-10-17 16:27:41 +0800

    feat: 构造函数

--- class/class.cpp
+++ class/class.cpp
@@ -15,20 +15,36 @@ struct Sales_data {
 
     // 关于sales_data对象的操作
     string isbn() const { return bookNo; }
+
     Sales_data &combine(const Sales_data &);
+
     double avg_price() const;
+
+    // 构造函数 C++11标准 生成默认的构造函数
+    Sales_data() = default;
+
+    // bookNo(s) 构造函数初始值列表 下同
+    Sales_data(const string &s) : bookNo(s) {}
+
+    Sales_data(const string &s, unsigned n, double p) : bookNo(s), units_sold(n), revenue(p * n) {}
+
+    Sales_data(istream &);
 };
 
 // Sales_data的非成员接口函数
 Sales_data add(const Sales_data &, const Sales_data &);
+
 ostream &print(ostream &, const Sales_data &);
+
 istream &read(istream &, Sales_data);
 // ===================定义抽象数据类型======================
 
 
-// ===============定义类相关的非成员函数===========
+// ===============构造函数===========
+// 默认构造函数无数任务实参 如果我们的类没有显式地定义构造函数，那么编译器就会为我们隐式地定义一个默认构造函数
+// 只有当类没有声明任何构造函数时，编译器才会自动地生成默认构造函数
 
-// ===============定义类相关的非成员函数===========
+// ===============构造函数===========
 
 int main(int argc, char **argv) {
     return 0;

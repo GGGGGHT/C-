1d9d409f75d3e538e99b1f8b66dd8ed4d6e2bfc3
ggggght <wz9712203617@gmail.com>
2021-10-18 11:33:04 +0800

    feat: 友元函数

--- class/class_access.cpp
+++ class/class_access.cpp
@@ -9,38 +9,55 @@ using namespace std;
  * 当希望定义类的所有成员是public的时，使用struct, 反之，如果希望成员是private的 使用class
  */
 class Sales_data {
+	// ========友元==========
+	// 为Sales_data的非成员函数所做的友元声明
+	friend Sales_data add(const Sales_data &, const Sales_data &);
+
+	friend istream &read(istream &, Sales_data &);
+
+	friend ostream &print(ostream &, const Sales_data &);
+	// ========友元==========
+
 	// 如何使用class关键字定义类 则aaa是无法被访问
 	// 如果使用struct 则aaa可以被访问到
 	// string aaa;
 public: // 访问说明符
-    // 构造函数 C++11标准 生成默认的构造函数
-    Sales_data() = default;
+	// 构造函数 C++11标准 生成默认的构造函数
+	Sales_data() = default;
 
-    // bookNo(s) 构造函数初始值列表 下同
-    Sales_data(const string &s) : bookNo(s) {}
+	// bookNo(s) 构造函数初始值列表 下同
+	Sales_data(const string &s) : bookNo(s) {}
 
-    Sales_data(const string &s, unsigned n, double p) : bookNo(s), units_sold(n), revenue(p * n) {}
+	Sales_data(const string &s, unsigned n, double p) : bookNo(s), units_sold(n), revenue(p * n) {}
 
-    Sales_data(istream &);
+	Sales_data(istream &);
 
-    // 关于sales_data对象的操作
-    string isbn() const { return bookNo; }
+	// 关于sales_data对象的操作
+	string isbn() const { return bookNo; }
 
-    Sales_data &combine(const Sales_data &);
+	Sales_data &combine(const Sales_data &);
 
 private: // 访问说明符
-    string bookNo;
-    unsigned units_sold = 0;
-    double revenue = 0.0;
+	string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
 
-    double avg_price() const {
-        return units_sold ? revenue / units_sold : 0;
-    }
+	double avg_price() const {
+		return units_sold ? revenue / units_sold : 0;
+	}
 };
 
+// ============Sales_data接口的非成员组成部分的声明
+Sales_data add(const Sales_data &, const Sales_data &);
+
+istream &read(istream &, Sales_data &);
+
+ostream &print(ostream &, const Sales_data &);
+// ===========Sales_data接口的非成员组成部分的声明
+
 int main(int argc, char **argv) {
-    Sales_data data;
-    // cout << "--> " << data.aaa << endl;
-    // cout << data.bookNo << endl; error无法访问 private
-    return 0;
+	Sales_data data;
+	// cout << "--> " << data.aaa << endl;
+	// cout << data.bookNo << endl; error无法访问 private
+	return 0;
 }

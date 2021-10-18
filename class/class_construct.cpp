b9535eae971a7c10a5f72642f767ad168333b2bb
ggggght <wz9712203617@gmail.com>
2021-10-18 21:52:40 +0800

    feat: 委托构造函数

--- class/class_construct.cpp
+++ class/class_construct.cpp
@@ -34,6 +34,20 @@ public:
 	X(int val): j(val), i(j) {}
 };
 
+class Sales_data {
+public:
+	// 非委托构造函数使用对应的实参初始化成员
+	Sales_data(std::string s,unsigned cnt,double price): bookNo(s), units_sold(cnt), revenue(cnt*price) {}
+	// 其余构造函数全部委托给另一个构造函数
+	Sales_data() : Sales_data("",0,0) {}
+	Sales_data(std::string s) : Sales_data(s,0,0) { }
+	// Sales_data(std::istream &is) : Sales_data() { read(is, *this); }
+
+private:
+	std::string bookNo;
+	unsigned units_sold;
+	double revenue;
+};
 // =====构造函数======
 int main(int argc, char **argv) {
 	ConstRef ref(1);

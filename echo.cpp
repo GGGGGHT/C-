d93c03906a8aaa573d22bebf67e60ac5699e46b1
ggggght <wz9712203617@gmail.com>
2021-10-20 22:55:34 +0800

    feat: commit

--- echo.cpp
+++ echo.cpp
@@ -1,7 +1,7 @@
 //#include <iostream>
 #include <stdio.h>
 
-int main() {
+int  main() {
     
 //    using namespace std;
 //
@@ -19,11 +19,22 @@ int main() {
 //    cout << endl;
 //    carrots = carrots - 1;
 //    cout << "Crunch, crunch. Now I have " << carrots << " crrots." << endl;
+//
+//    int a = 65;
+//    int addr;
+//    const double pi = 3.14;
+//    double *ptr = &pi;
+//    const double *cptr = &pi;
+//    double dval = 3.14;
+//    cptr = &dval;
+//    addr = 0x0012ff7c;
+//    printf("0x%p, 0x%p, 0x%p\n", &a, addr, (int*)addr);
+//    printf("%d,%d,%d\n", a, *&a, *(int *)addr); // 65 65 0x0012ff7c
 
-    int a = 65;
-    int addr;
-
-    addr = 0x0012ff7c;
-    printf("0x%p, 0x%p, 0x%p\n", &a, addr, (int*)addr);
-    printf("%d,%d,%d\n", a, *&a, *(int *)addr); // 65 65 0x0012ff7c
+//    typedef char *pstring;
+ //   const pstring cstr = 0;
+  //  const pstring *ps;
+   // printf("-------\n");
+   //  printf( "cstr: %c\n", *cstr);
+    printf("sizeof(unsigned char) = %lu",sizeof(unsigned char));
 }

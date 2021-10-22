63c393c8ace4ec25b0f95eac6023779b6371061f
ggggght <wz9712203617@gmail.com>
2021-10-22 22:11:47 +0800

    feat:  泛型算法-写入操作

--- generic_algorithm/generic_algorithm.cpp
+++ generic_algorithm/generic_algorithm.cpp
@@ -2,6 +2,8 @@
 #include <numeric>
 #include <vector>
 #include <iostream>
+#include <iterator>
+
 
 using namespace std;
 
@@ -16,4 +18,20 @@ int main() {
 	vector<string> strs{"he", "ll", "o ", "wor", "ld"};
 	// 将空串当做一个字符串字面值传递给第三个参数是不可以的 因为 const char*上没有定义 + 运算符
 	std::cout << accumulate(strs.cbegin(), strs.cend(), string{""}) << std::endl;
+
+	// 将给定的这个值赋予输入序列中的每个元素
+	fill(vec.begin(), vec.end(), 33);
+	for (auto cb = vec.cbegin(), ce = vec.cend(); cb != ce;) {
+		std::cout << *cb++ << std::endl;
+	}
+
+	vector<int> v;
+	auto iter = back_inserter(v);
+	fill_n(iter, 10, 2); // 使用插入迭代器,向v中添加10个0
+	std::cout << "size: " << v.size() << ",capacity: " << v.capacity() << std::endl;
+	/*std::cout << "size: " << v.size() << ",capacity: " << v.capacity() << std::endl;
+	*iter = 42; // v中现在包含一个元素 42
+	*iter = 33;
+	*iter = 34;
+	std::cout << "size: " << v.size() << ",capacity: " << v.capacity() << std::endl;*/
 }

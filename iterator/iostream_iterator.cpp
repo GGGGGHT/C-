22e88485db137532db7ea81b92f8f462dc77c5f2
ggggght <wz9712203617@gmail.com>
2021-10-24 03:30:20 -0700

    feat: iostream

---
+++ iterator/iostream_iterator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/**
+ * istream_iterator 读取输入流
+ * ostream_iterator 向一个输出流写数据
+ *
+ * 当创建一个流迭代器时，必须指定迭代器将要读写的对象类型。
+ * @return
+ */
+int main() {
+    std::cout << "h" << std::endl;
+}

3ee7ebd9b7ccd642bdd1d1dac644aca6a62dd10e
ggggght <wz9712203617@gmail.com>
2021-10-24 03:57:56 -0700

    feat: 反向迭代器

--- iterator/iostream_iterator.cpp
+++ iterator/iostream_iterator.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iterator>
 #include <vector>
+
 using namespace std;
 
 /**
@@ -10,7 +11,7 @@ void readFromStandardIn() {
     istream_iterator<int> in_iter(cin); // 从cin读取int
     istream_iterator<int> eof;
     // 可以使用元素范围迭代器来构造vec
-    vector<int> vec(in_iter,eof);
+    vector<int> vec(in_iter, eof);
 //    while (in_iter != eof) {
 //        // 后置递增运算符读取流，返回迭代器的旧值
 //        // 解引用迭代器，获得从流读取的前一个值
@@ -40,6 +41,20 @@ void writeToOstream() {
     }
     cout << endl;
 }
+
+/**
+ * 反向迭代器
+ * 反向迭代器就是在容器中从尾元素向首元素反向移动的迭代器。
+ * 对于反向迭代器，递增（以及递减）操作的含义会颠倒过来。递增一个反向迭代器（++it）会移动到前一个元素；
+ * 递减一个迭代器（--it）会移动到下一个元素。
+ */
+void reverseIterator() {
+    vector<int> v = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for (auto r_iter = v.crbegin(); r_iter != v.crend(); ++r_iter) {
+        cout << *r_iter << endl;
+    }
+}
+
 /**
  * istream_iterator 读取输入流
  * ostream_iterator 向一个输出流写数据
@@ -49,5 +64,6 @@ void writeToOstream() {
  */
 int main() {
 //    readFromStandardIn();
-    writeToOstream();
+//    writeToOstream();
+    reverseIterator();
 }

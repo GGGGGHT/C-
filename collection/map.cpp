75537b719220113abe0d2c608674459b3ba21321
ggggght <wz9712203617@gmail.com>
2021-10-26 14:44:41 +0800

    feat:  map insert的方式

--- collection/map.cpp
+++ collection/map.cpp
@@ -6,12 +6,25 @@
 using namespace std;
 
 void useMap() {
-	map<string,int>::value_type v1;
-	map<string,int>::key_type  v2;
-	map<string,int>::mapped_type  v3;
-	cout << v1 << endl;
-	cout << v2 << endl;
-	cout << v3 << endl;
+	map<string,int> word_count;
+	word_count.insert({"world",2});
+	word_count.insert(pair<string,int>("hello", 1));
+	word_count.insert(make_pair("ght", 1));
+	word_count.insert(map<string, int>::value_type("ggg", 5));
+	// word_count.insert();
+	// map<string,int>::value_type v1;
+	// map<string,int>::key_type  v2;
+	// map<string,int>::mapped_type  v3;
+	// cout << v1 << endl;
+	// cout << v2 << endl;
+	// cout << v3 << endl;
+	// auto world = word_count.find("world");
+	// ++world->second;
+	auto b = word_count.cbegin(), e = word_count.cend();
+	while (b != e) {
+		cout << b->first << " -> " << b->second << endl;
+		++b;
+	}
 }
 
 void usePair() {
@@ -30,5 +43,6 @@ void useSet() {
 
 int main() {
 	// usePair();
-	useSet();
+	// useSet();
+	useMap();
 }

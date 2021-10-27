6988edb4fc2678dd41bb2fa3c20210ccff4163ca
ggggght <wz9712203617@gmail.com>
2021-10-27 14:29:33 +0800

    feat:  use map

---
+++ collection/transfer.cpp
@@ -0,0 +1,54 @@
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+map<string, string> buildMap(ifstream &map_file) {
+	map<string,string> trans_map;
+	string key,value;
+	while (map_file >> key && getline(map_file, value)) {
+		if(value.size() > 1) {
+			trans_map[key] = value.substr(1);
+		} else {
+			throw runtime_error("no rule for " + key);
+		}
+	}
+
+	return trans_map;
+}
+
+const string &transform(const string &s, const map<string, string> &m) {
+	auto map_it = m.find(s);
+	if (map_it != m.end()) {
+		return map_it->second;
+	} else {
+		return s;
+	}
+}
+
+void word_transform(ifstream &map_file, ifstream &input) {
+	auto trans_map = buildMap(map_file);
+	string text;
+	while (getline(input, text)) {
+		std::istringstream stream(text);
+		string word;
+		bool firstWord = true;
+		while (stream >> word) {
+			if(firstWord) {
+				firstWord = !firstWord;
+			} else {
+				cout << " ";
+			}
+
+			cout << transform(word, trans_map);
+		}
+		cout << endl;
+	}
+}
+int main() {
+	ifstream map_file("mapfile");
+	ifstream input("input");
+
+	word_transform(map_file, input);
+}

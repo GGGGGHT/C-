ddd3da573f688377d0a28704a25bf8e451f7c3ab
Autorun <20293349@qq.com>
2021-10-28 14:33:43 +0800

    王总牛批

--- object/abstract_polymorphic.cpp
+++ object/abstract_polymorphic.cpp
@@ -7,8 +7,10 @@
 // Created by autorun on 2021/11/8.
 //
 #include "abstract_polymorphic.h"
+#include "../common/common.h"
 
 int main() {
     abstract_polymorphic* po1 = new impl1_polymorphic;
     po1->test();
+
 }

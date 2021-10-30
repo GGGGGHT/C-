7cf5832bd309832c8c2847cc352e8e85a037ea29
error0702 <20293349@qq.com>
2021-10-30 11:43:29 +0800

    add test

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,11 @@
 //
 // Created by autorun on 2021/10/30.
 //
-#include "calc.h"
+#include "calc/calc.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+    // add test
+    add(1, 2);
     return 0;
 }

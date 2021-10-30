c58c5c4b2d37cd7bb9a3e1b062e0444575e9c127
error0702 <20293349@qq.com>
2021-10-30 11:47:34 +0800

    remove main add calc

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -1,8 +1,8 @@
 //
 // Created by autorun on 2021/10/30.
 //
-
 #include "calc.h"
+#include <cassert>
 
 int add(int i, int j)
 {
@@ -12,4 +12,14 @@ int add(int i, int j)
 int sub(int i, int j)
 {
     return i - j;
+}
+
+
+
+int main()
+{
+    // add test
+    auto rst = add(1, 2);
+    assert(rst == 3);
+    return 0;
 }

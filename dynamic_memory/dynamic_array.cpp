3b9816be642644025dac103b97a4f0c4968f5407
ggggght <wz9712203617@gmail.com>
2021-10-31 11:32:50 +0800

    feat: 动态数组

--- dynamic_memory/dynamic_array.cpp
+++ dynamic_memory/dynamic_array.cpp
@@ -17,10 +17,20 @@ int main() {
     // auto pb = begin(p), pe = end(p);
     // auto b = begin(a), e = end(a);
     // while (b != e) {
-        // cout << *b++ << endl;
+    // cout << *b++ << endl;
     // }
-    cout << "size of int:  " << sizeof (int) << endl;
+    cout << "size of int:  " << sizeof(int) << endl;
     for (int i = 0; i != 41; ++i) {
-        cout << "index: " << i << ", value: " <<  *(p++) << ", memory: " << p++ << endl;
+        cout << "index: " << i << ", value: " << *(p++) << ", memory: " << p++ << endl;
     }
+
+    // 释放动态数组
+    // delete p; p 必须指向一个动态分配的对象或为空
+    // 当我们释放一个指向数组的指针时,空方括号对是必须的!
+    delete[] p; // p必须指向一个动态分配的数组或为空 数组中的元素按逆序销毁,即最后一个元素首先被销毁
+
+    typedef int arrT[42];
+    int *pa = new arrT;
+    // 不管外表如何，pa指向一个对象数组的首元素，而不是一个类型为arrT的单一对象。因此，在释放p时我们必须使用[]。
+    delete[] pa;
 }

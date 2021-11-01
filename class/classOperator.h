026ba55c792808eec8196f04bade0eeed9ad464a
ggggght <wz9712203617@gmail.com>
2021-11-01 23:10:46 +0800

    feat: 运算符重载

--- class/classOperator.h
+++ class/classOperator.h
@@ -15,6 +15,9 @@ public :
     Foo(); // 默认构造函数
 
     Foo(const Foo &); // 如果一个构造函数的第一个参数是自身类类型的引用，且任何额外参数都有默认值，则此构造函数是拷贝构造函数。
+
+    // 重载运算符本质上是函数，其名字由operator关键字后接表示要定义的运算符的符号组成。
+    Foo &operator=(const Foo &); // 重载赋值运算符
 };
 
 #endif //C__CLASSOPERATOR_H

dbb9110323bbd4b4f58d585a11ec5f7464613dcc
error0702 <20293349@qq.com>
2021-11-01 14:10:50 +0800

    shared_ptr

---
+++ dynamic_point/smart_point/shared_ptr.cpp
@@ -0,0 +1,12 @@
+//
+// Created by autorun on 2021/11/1.
+//
+
+#include "shared_ptr.h"
+
+int main(int argc, char *argv[]) {
+    shared_ptr<string> sPtr;
+
+    printf("sizeOf: %lu", sizeof(sPtr));
+    return 0;
+}
---
+++ dynamic_point/smart_point/shared_ptr.h
@@ -0,0 +1,19 @@
+//
+// Created by autorun on 2021/11/1.
+//
+
+#ifndef C__DEMO_H
+#include <string>
+#include "stdio.h"
+#include <iostream>
+using namespace std;
+
+#define C__DEMO_H
+
+
+class demo {
+
+};
+
+
+#endif //C__DEMO_H

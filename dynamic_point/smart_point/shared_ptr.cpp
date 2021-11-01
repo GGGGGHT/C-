0ba087f67ca11bfbb80074230db389535abed7e9
Autorun <20293349@qq.com>
2021-11-01 21:40:06 +0800

    update shared_ptr.cpp

--- dynamic_point/smart_point/shared_ptr.cpp
+++ dynamic_point/smart_point/shared_ptr.cpp
@@ -4,9 +4,20 @@
 
 #include "shared_ptr.h"
 #include <memory>
+
 int main(int argc, char *argv[]) {
     shared_ptr<string> sPtr;
 
-    printf("sizeOf: %lu", sizeof(sPtr));
+    printf("sizeOf: %lu \n", sizeof(sPtr));
+
+//    if (sPtr && sPtr->empty()) {
+//        *sPtr = "hi";
+//    }
+//
+//    printf("%s\n", sPtr.get());
+    sPtr = make_shared<string>("hello world!");
+    cout << sPtr << endl;
+    cout << &sPtr << endl;
+    cout << *sPtr << endl;
     return 0;
 }

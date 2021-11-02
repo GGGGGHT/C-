bbf5eb2aeaf6b49c54afb356727d423d7e18e4e0
Autorun <20293349@qq.com>
2021-11-02 23:11:35 +0800

    placeholder_test.cpp

---
+++ placeholder/placeholder_test.cpp
@@ -0,0 +1,17 @@
+//
+// Created by autorun on 2021/11/2.
+//
+
+#include "placeholder_test.h"
+
+// 占位参数
+int fun(int, int = 100)
+{
+
+}
+
+int main()
+{
+
+
+}
---
+++ placeholder/placeholder_test.h
@@ -0,0 +1,15 @@
+//
+// Created by autorun on 2021/11/2.
+//
+
+#ifndef C__PLACEHOLDER_TEST_H
+#include "../common/common.h"
+#define C__PLACEHOLDER_TEST_H
+
+
+class placeholder_test {
+
+};
+
+
+#endif //C__PLACEHOLDER_TEST_H

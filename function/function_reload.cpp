fa99ccb44590a27b09b9a99408c5cfbb0186dbd9
Autorun <20293349@qq.com>
2021-11-02 23:18:48 +0800

    function reload

---
+++ function/function_reload.cpp
@@ -0,0 +1,32 @@
+//
+// Created by autorun on 2021/11/2.
+//
+
+#include "function_reload.h"
+
+// 函数重载
+// 1. 同一作用域下。
+// 2. 函数名相同
+// 3. 参数类型不同，个数、顺序不同
+void fun()
+{
+
+}
+void fun(int i)
+{
+
+}
+
+void fun(int, double)
+{
+
+}
+
+void fun(double, int)
+{
+
+}
+int main()
+{
+
+}
---
+++ function/function_reload.h
@@ -0,0 +1,15 @@
+//
+// Created by autorun on 2021/11/2.
+//
+
+#ifndef C__FUNCTION_RELOAD_H
+#include "../common/common.h"
+#define C__FUNCTION_RELOAD_H
+
+
+class function_reload {
+
+};
+
+
+#endif //C__FUNCTION_RELOAD_H

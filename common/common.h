936c3c88301cb35e0c4a0416de59d1cf56fe1526
error0702 <20293349@qq.com>
2021-11-02 11:56:25 +0800

    add pringLog

--- common/common.h
+++ common/common.h
@@ -1,10 +1,49 @@
 //
 // Created by autorun on 2021/11/1.
 //
+#include "stdio.h"
 #include <iostream>
-
 using namespace std;
 #ifndef C__COMMON_H
 #define C__COMMON_H
 
 #endif //C__COMMON_H
+
+/*=================================
+ *  自定义打印输出
+ ==================================*/
+#define INFO_OUTPUT         3
+#define WARNING_OUTPUT      2
+#define DEBUG_OUTPUT        1
+#define ERROR_OUTPUT        0
+
+#define DEBUG
+#define DEBUG_LEVEL         INFO_OUTPUT
+
+#define PRINT(info, ...) do{ \
+    printf("[Info] (%s:%d->%s):" info"", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
+}while(0)
+
+#define INFO(info, ...) do{ \
+    if(DEBUG_LEVEL>=INFO_OUTPUT){ \
+        printf("[Info] (%s:%d->%s):" info"", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
+    } \
+}while(0)
+
+#define WARNING(info, ...) do{ \
+    if(DEBUG_LEVEL>=WARNING_OUTPUT){ \
+        printf("[Warning] (%s:%d->%s):" info"", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
+    } \
+}while(0)
+
+#define DEBUG(info,...) do{ \
+    if(DEBUG_LEVEL>=DEBUG_OUTPUT){ \
+        printf("[Debug] (%s:%d->%s):" info"",__FILE__,__FUNCTION__,__LINE__,##__VA_ARGS__); \
+    } \
+}while(0)
+
+#define ERROR(info, ...) do{ \
+	if(DEBUG_LEVEL>=ERROR_OUTPUT){ \
+    	printf("[Error] (%s:%d->%s):" info"", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
+    } \
+}while(0)

3bfbe62bd77b0dce251cd264cd561bae1b8fa145
Autorun <20293349@qq.com>
2021-11-07 23:49:30 +0800

    add polymorphic

---
+++ object/polymorphic.cpp
@@ -0,0 +1,5 @@
+//
+// Created by autorun on 2021/11/7.
+//
+
+#include "polymorphic.h"
---
+++ object/polymorphic.h
@@ -0,0 +1,14 @@
+//
+// Created by autorun on 2021/11/7.
+//
+
+#ifndef C__POLYMORPHIC_H
+#define C__POLYMORPHIC_H
+
+
+class polymorphic {
+
+};
+
+
+#endif //C__POLYMORPHIC_H

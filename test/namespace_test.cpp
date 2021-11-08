3f7f4f60747c4a00424694ea3b5d44f84ef9458c
error0702 <20293349@qq.com>
2021-11-08 17:53:52 +0800

    add namespace

--- test/namespace_test.cpp
+++ test/namespace_test.cpp
@@ -1,14 +1,5 @@
-// Copyright (c) 2021. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-// Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
-// Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
-// Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
-// Vestibulum commodo. Ut rhoncus gravida arcu.
 //
-// Created by autorun on 2021/10/31.
+// Created by autorun on 2021/11/8.
 //
-#include <iostream>
-int main()
-{
-    std::cout << "hello world!" << std::endl;
-    return 0;
-}
+
+#include "namespace_test.h"
---
+++ test/namespace_test.h
@@ -0,0 +1,14 @@
+//
+// Created by autorun on 2021/11/8.
+//
+
+#ifndef C__NAMESPACE_TEST_H
+#define C__NAMESPACE_TEST_H
+
+
+class namespace_test {
+
+};
+
+
+#endif //C__NAMESPACE_TEST_H

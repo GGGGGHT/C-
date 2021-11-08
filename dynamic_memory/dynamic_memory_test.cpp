10740f5d561aedeca2f18471dddbbc25f8d5ae4f
error0702 <20293349@qq.com>
2021-11-08 17:39:52 +0800

    update 2

--- dynamic_memory/dynamic_memory_test.cpp
+++ dynamic_memory/dynamic_memory_test.cpp
@@ -9,14 +9,25 @@
 
 #include "dynamic_memory_test.h"
 
-int main() {
-    double *pvalue = NULL; // 初始化为 null 的指针
-    pvalue = new double;// 为变量请求内存
-    if (!(pvalue = new double ))
-    {
+void allow(double *pvalue) {
+    pvalue = new double;
+    if (pvalue == nullptr) {
         INFO("alloc memory fail.");
         exit(1);
     }
-    delete pvalue;
+}
+
+
+int main() {
+    double *pvalue = NULL; // 初始化为 null 的指针
+//    pvalue = new double;// 为变量请求内存
+    // 如果自由存储区已被用完，可能无法成功分配内存。所以建议检查 new 运算符是否返回 NULL 指针，并采取以下适当的操作：
+
+//    allow(pvalue);
+    pvalue = new double;
+    *pvalue = 3.1415926;
+//    INFO("value: \f\n", *pvalue);
+    cout << *pvalue << endl;
+    delete pvalue; // 释放 pvalue 所指向的内存
     return 0;
 }

7df083bfe95e4433aea22b4269efb248ce2f2733
error0702 <20293349@qq.com>
2021-11-08 18:16:03 +0800

    add template

---
+++ template/template_test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by autorun on 2021/11/8.
+//
+
+#include "template_test.h"
---
+++ template/template_test.h
@@ -0,0 +1,14 @@
+//
+// Created by autorun on 2021/11/8.
+//
+
+#ifndef C__TEMPLATE_TEST_H
+#define C__TEMPLATE_TEST_H
+
+
+class template_test {
+
+};
+
+
+#endif //C__TEMPLATE_TEST_H

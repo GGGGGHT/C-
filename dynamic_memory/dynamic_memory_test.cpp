92c4c239ec15eadac0b39c55f1dc437e9909ceba
error0702 <20293349@qq.com>
2021-11-08 17:27:34 +0800

    add

---
+++ dynamic_memory/dynamic_memory_test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by autorun on 2021/11/8.
+//
+
+#include "dynamic_memory_test.h"
---
+++ dynamic_memory/dynamic_memory_test.h
@@ -0,0 +1,14 @@
+//
+// Created by autorun on 2021/11/8.
+//
+
+#ifndef C__DYNAMIC_MEMORY_TEST_H
+#define C__DYNAMIC_MEMORY_TEST_H
+
+
+class dynamic_memory_test {
+
+};
+
+
+#endif //C__DYNAMIC_MEMORY_TEST_H

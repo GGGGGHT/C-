64f77e07629b9616b9ccf7ab342d325f8b163fa0
error0702 <20293349@qq.com>
2021-11-10 15:19:05 +0800

    add class_template_test.cpp

---
+++ template/class_template_test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by autorun on 2021/11/10.
+//
+
+#include "class_template_test.h"
---
+++ template/class_template_test.h
@@ -0,0 +1,14 @@
+//
+// Created by autorun on 2021/11/10.
+//
+
+#ifndef C__CLASS_TEMPLATE_TEST_H
+#define C__CLASS_TEMPLATE_TEST_H
+
+
+class class_template_test {
+
+};
+
+
+#endif //C__CLASS_TEMPLATE_TEST_H

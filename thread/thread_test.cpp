6dbdac13057bff6db4b3a6b6db4148dd5e07183a
error0702 <20293349@qq.com>
2021-11-10 11:07:05 +0800

    update total

--- thread/thread_test.cpp
+++ thread/thread_test.cpp
@@ -16,8 +16,9 @@
 
 //线程函数
 void *test(void *ptr) {
+    int total = 10;
     int i;
-    for (i = 0; i < 8; i++) {
+    for (i = 0; i < total; i++) {
         printf("the pthread running ,count: %d\n", i);
         sleep(1);
     }

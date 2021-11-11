088f4a420b6b26b1aa448d9401d9efc657b313e7
Autorun <20293349@qq.com>
2021-11-11 21:52:48 +0800

    format

--- object/const_obj_fun.cpp
+++ object/const_obj_fun.cpp
@@ -7,5 +7,4 @@
 //
 // Created by autorun on 2021/11/4.
 //
-
 #include "const_obj_fun.h"
--- object/const_obj_fun.h
+++ object/const_obj_fun.h
@@ -6,7 +6,6 @@
 //
 // Created by autorun on 2021/11/4.
 //
-
 #ifndef C__CONST_OBJ_FUN_H
 #include "../common/common.h"
 #define C__CONST_OBJ_FUN_H

a911ff62f1bd3830c341071540da4e043ea66c11
ggggght <wz9712203617@gmail.com>
2021-11-14 17:43:29 +0800

    feat: 获取tuple的元素类型和大小

--- standard_libary/tuple_test.cpp
+++ standard_libary/tuple_test.cpp
@@ -17,4 +17,20 @@ int main() {
                                                                               {0, 1, 2, 3, 4});
     // tuple<const char＊，int，double>
     auto item = std::make_tuple("0-999-123-X",3,20.00);
+    using namespace std;
+    auto book = get<0>(item);
+    auto cnt = get<1>(item);
+    auto price = get<2>(item);
+    std::cout << book << std::endl;
+    std::cout << cnt << std::endl;
+    std::cout << price << std::endl;
+    get<2>(item) *= 0.8;
+    std::cout << get<2>(item) << std::endl;
+    // trans是item的类型
+    typedef decltype(item) trans;
+    // 返回trans类型对象中成员的数量
+    size_t sz = tuple_size<trans>::value;
+    cout << sz << endl;
+    tuple_element<1, trans>::type cnt1 = get<1>(item);
+    cout << cnt1 << endl;
 }

0f779655827060e46991c5acfc577a9b4d9248b0
Autorun <20293349@qq.com>
2021-11-16 00:10:17 +0800

    format

--- friend/friend_test.cpp
+++ friend/friend_test.cpp
@@ -1,7 +1,6 @@
 //
 // Created by autorun on 2021/11/4.
 //
-
 #include "friend_test.h"
 
 int main()
@@ -11,5 +10,4 @@ int main()
 //    t1.i = 500;
     test1(t1);
     INFO("value: %d\n", t1.getI());
-
 }

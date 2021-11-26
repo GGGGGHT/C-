e2e0e9a24ba7358221ee48bf1ad119684cf746a2
ggggght <wz9712203617@gmail.com>
2021-11-26 15:55:29 +0800

    feat: 内联汇编

--- asm/asm.c
+++ asm/asm.c
@@ -21,3 +21,19 @@ int main() {
     printf("Result: %d, %d\n", a, b);
     return 0;
 }
+
+
+
+inline jint     Atomic::cmpxchg    (jint     exchange_value, volatile jint*     dest, jint     compare_value) {
+  int mp = os::is_MP();
+  __asm__ volatile (LOCK_IF_MP(%4) "cmpxchgl %1,(%3)"
+                   : "=a" (exchange_value)
+                   : "r" (exchange_value), "a" (compare_value), "r" (dest), "r" (mp)
+                   : "cc", "memory");
+  return exchange_value;
+}
+// 第一个冒号后跟的是输出 "=a" (exchange_value) 输出
+// 第二个冒号后跟的是输入  "r" (exchange_value), "a" (compare_value), "r" (dest), "r" (mp) 输入 输入参数有4个
+// 第三个冒号表示clob 表示可能会影响寄存器 cc寄存器
+// %1 表示 exchange_value
+// %3 表示 dest

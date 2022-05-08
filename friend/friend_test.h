dea290bfa5056ce90b5380f2d59acbea4121d4b3
Autorun <20293349@qq.com>
2022-05-08 17:18:05 +0800

    update

--- friend/friend_test.h
+++ friend/friend_test.h
@@ -6,11 +6,9 @@
 //
 // Created by autorun on 2021/11/4.
 //
-
 #ifndef C__FRIEND_TEST_H
 #include "../common/common.h"
 #define C__FRIEND_TEST_H
-
 
 class friendTest {
     friend void test1(friendTest &test);
